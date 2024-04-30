4dabf5cecc4f6b3dce17690937e280101c789d34
NextIn <156282119+472022@users.noreply.github.com>
2024-04-30 16:21:57 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBXiynEKZWTg6zGk5lPOy-wK79vkFNPk2c",
+            authDomain: "portfolio-c1012.firebaseapp.com",
+            projectId: "portfolio-c1012",
+            storageBucket: "portfolio-c1012.appspot.com",
+            messagingSenderId: "547075714430",
+            appId: "1:547075714430:web:192d1336b8b107adb1972f"
+};
+
+// Initialize Firebasea
